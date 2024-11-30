5ebb51ea8c3db4f90058c2110346f23736045419
josaPlus <josafat.saldana@hotmial.comm>
2024-11-29 19:47:35 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyectoFinal5_1',
+  webDir: 'www'
+};
+
+export default config;
